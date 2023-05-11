9c1192470ffd5504894a9f0f22d0ea225b33ea04
Milan9135 <Milpower0@gmail.com>
2023-05-12 00:08:01 +0200

    create opdracht.php
    E A CODE

---
+++ opdracht.php
@@ -0,0 +1,25 @@
+<?php
+
+$host = "localhost:3307";
+$username = "root";
+$password = "";
+$database = "winkel1";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected to database $database";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+$stmt = $conn->query('SELECT @@version');
+
+
+
+// while ($row = $stmt->fetch())
+// {
+//     echo $row[''] . "<br>";
+// }
